90cab9b2bec16d84a22965e78df24e5267b2fe69
onikun94 <onikun94@gmail.com>
2021-08-27 23:58:39 +0900

    abc214

--- abc214/c/main.py
+++ abc214/c/main.py
@@ -7,6 +7,7 @@ sunukeSave = [0] * N
 
 currentSunuke = t.index(min(t))
 sunuke[currentSunuke] = min(t)
+currentMin = min(t)
 sunukeSave[currentSunuke] = min(t)
 t[currentSunuke] = MAX
 
@@ -14,16 +15,19 @@ t[currentSunuke] = MAX
 while currentSunuke < N - 1:
     takaMin = min(t)
     takaIndexMin = t.index(min(t))
-
-    if sunuke[currentSunuke] + s[currentSunuke] < takaMin:
+    if takaMin == currentMin:
+        sunuke[takaIndexMin] = takaMin
+        sunukeSave[takaIndexMin] = takaMin
+        t[takaIndexMin] = MAX
+    elif sunuke[currentSunuke] + s[currentSunuke] < takaMin:
         if sunuke[currentSunuke + 1] == 0:
             sunukeSave[currentSunuke + 1] = sunuke[currentSunuke] + \
                 s[currentSunuke]
         sunuke[currentSunuke + 1] = sunuke[currentSunuke] + s[currentSunuke]
         currentSunuke += 1
     else:
-        if sunuke[currentSunuke + 1] == 0:
-            sunukeSave[currentSunuke + 1] = takaMin
+        if sunuke[takaIndexMin] == 0:
+            sunukeSave[takaIndexMin] = takaMin
         sunuke[takaIndexMin] = takaMin
         t[takaIndexMin] = MAX
 

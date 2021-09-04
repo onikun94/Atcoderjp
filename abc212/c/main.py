3eaa4eda5f1be97fbd480d35409299de692749b7
onikun94 <onikun94@gmail.com>
2021-09-04 22:16:58 +0900

    abc212

---
+++ abc212/c/main.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans1 = 0
+ans2 = ''
+ans3 = 0
+if max(A) > max(B):
+    ans1 = max(B)
+    ans2 = 'A'
+else:
+    ans1 = max(A)
+    ans2 = 'B'
+
+ans = 1000000000
+if ans2 == 'A':
+    for i in A:
+        ans = min(ans, abs(ans1 - i))
+elif ans2 == 'B':
+    for i in B:
+        ans = min(ans, abs(ans1-i))
+
+print(ans)

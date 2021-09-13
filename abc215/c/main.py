46dd8cbf39caf33225f7801534998cf633f8b9ea
onikun94 <onikun94@gmail.com>
2021-09-13 17:31:17 +0900

    abc215

--- abc215/c/main.py
+++ abc215/c/main.py
@@ -1,18 +1,18 @@
 import itertools
-S = input()
-K = int(input())
+S, k = map(str, input().split())
+K = int(k)
 count = 0
 data = set()
+dic = []
 for nums in itertools.permutations(range(len(S))):
-    print("nums = ", nums)
-    tmp = ""
-    for i in range(len(nums)):
-        print("i = ", i)
-        tmp = tmp + S[nums[i]]
-        data.add(tmp)
-
-ans = list(data)
-print("ans = ", ans)
-ans.sort()
-
+    dic.append(nums)
+dicans = []
+for i in dic:
+    word = ""
+    for j in i:
+        word += S[j]
+    dicans.append(word)
+dicans.sort()
+ans = list(dict.fromkeys(dicans))
+print(ans)
 print(ans[K-1])

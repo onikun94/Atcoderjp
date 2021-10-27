8fa35c91591be43cee3470d26b13f31d9710d8d9
onikun94 <onikun94@gmail.com>
2021-10-28 05:46:27 +0900

    abc218

---
+++ abc218/a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    string S;
+    cin >> N >> S;
+    if(S[N - 1] == 'o') cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}

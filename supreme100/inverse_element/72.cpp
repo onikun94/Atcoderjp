88c98fb36eca673343716dc2d9f1def2524507ff
onikun94 <onikun94@gmail.com>
2021-12-18 09:47:36 +0900

    supreme72

---
+++ supreme100/inverse_element/72.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define REP(i,x,y) for(ll i = (ll)x; i < (ll)y; ++i)
+
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+ll fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit(){
+  fac[0] = fac[1] = 1;
+  finv[0] =finv[1] = 1;
+  inv[1] = 1;
+  for(int i = 2; i < MAX; ++i){
+    fac[i] = fac[i-1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+}
+
+ll COM(int n, int k){
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main(){
+  int w,h; cin >> w >> h;
+  COMinit();
+  cout << COM((w-1)+(h-1),h-1) << endl;
+
+}
+
